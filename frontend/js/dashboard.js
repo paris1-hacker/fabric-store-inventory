e8128a4b2257dbb9a859d4ec516d0ae70d76730a
Ofem <ofempatrick10@gmail.com>
2026-08-10 18:55:46 +0100

    removed unnecessary code blocks and improve sidebar functionality

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -11,6 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
     loadDashboard();
 
 });
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+
+if (menuToggle && sidebar) {
+    menuToggle.addEventListener("click", () => {
+        sidebar.classList.toggle("show");
+    });
+}
 
 
 /*
